2ce8cb28cd76d67d8d125336acbfb4bff45e47ee
Lane Pentecost <30759236+Project-Faust@users.noreply.github.com>
2025-01-09 19:54:45 +0000

    updated minor content and styling for example structure

--- app/page.js
+++ app/page.js
@@ -1,15 +1,22 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Welcome to QA Jewelry. You may have questions; we may have answers.
-          </li>
-          <li>Go to our products page to check out our gneiss schist.</li>
-        </ol>
+    <div className="max-w-4x1 mx-auto p-6">
+      <header>
+        <h1 className="text-4x1 font-bold text-center text-gray-800 mb-6">
+          Welcome to QA Rock Shop
+        </h1>
+        <p className="text-xl text-center text-gray-600 mb-8">
+          You may have questions, we may have answers.
+        </p>
+      </header>
+      <main>
+        <a
+        href="/products"
+        className="bg-blue-600 text-white px-8 py-3 rounded-full text-xl hover:bg-blue-700 transition duration-300">
+          Visit our products page to check out our gneiss schist.
+        </a>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer>
       </footer>
     </div>
   );
